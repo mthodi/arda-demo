377a2acb76fcf2c5aa748a3b8f004cabf1cf00b4
Martin Thodi <martthodi238@gmail.com>
2023-08-24 11:52:11 +0000

    Update AS view table

--- src/routes/region/ASRelations/RegionASView.js
+++ src/routes/region/ASRelations/RegionASView.js
@@ -37,7 +37,24 @@ export default function RegionASView ({ regionId }){
         </Col>
       </Row>
       <Row>
-        <Col span={24}><ASHeatmap /></Col>
+        {/* <Col span={24}><ASHeatmap /></Col> */}
+        <Col span={6}></Col>
+        <Col span={12} style={{ textAlign: 'center', marginBottom: 2, marginTop: 10}}>
+          <Text style={{ display: 'block', textAlign: 'center', marginBottom: 20 }}>
+            Understanding how different autonomous systems (AS) do business
+            together is crucial for both the technical and economic aspects
+            of the Internet's structure. When ASs establish relationships,
+            it affects how internet traffic flows, causing certain restrictions
+            on the paths traffic can take. These findings have a big impact on
+            various aspects of how networks work and are studied, like making
+            networks more stable, optimizing how data moves, figuring out
+            the bigger picture of network structures, and other things
+            that matter both in research and practical use.
+
+            We will show AS relationships based on CAIDA's AS Relationships datasets.
+          </Text>
+        </Col>
+        <Col span={6}></Col>
       </Row>
     </>
   );
